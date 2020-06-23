b26e9b3fff58171253a2366d7370779b2d35457a
Bibek Adhikari <bibekadhikari2141@gmail.com>
2020-06-23 14:08:36 -0700

    completed project

--- app.js
+++ app.js
@@ -26,7 +26,15 @@ app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+// app.listen(
+//   PORT,
+//   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+// );
+
+app.listen(PORT, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
